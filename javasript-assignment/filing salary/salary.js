1437bc8548ee3f66646cc20bef356ee1acd72b2c
ayecaptain <pkemoimethodius@gmail.com>
2024-07-05 00:59:00 +0300

    The net salary calculato

---
+++ javasript-assignment/filing salary/salary.js
@@ -0,0 +1,27 @@
+function grossSalary(basic,allowances,compensation){
+    return basic+allowances+compensation;
+}
+
+
+function deductions(loan,insuarance){
+  return loan+insuarance;
+}
+
+
+function netSalary(grossSalary,deductions){
+  if (grossSalary < 10000){
+    tax=0;
+  }else if(grossSalary > 10000 && grossSalary < 70000){
+    tax=grossSalary*0.11;
+  }else if(grossSalary > 70000 && grossSalary < 100000){
+    tax=grossSalary*0.13;
+  }else if(grossSalary > 100000 && grossSalary < 150000){
+    tax=grossSalary*0.15;
+  }else if(grossSalary > 150000 && grossSalary < 200000){
+    tax=grossSalary*0.17;
+  }else{
+    tax=grossSalary*0.19;
+  }
+  return grossSalary-tax-deductions;
+}
+
